59bb4abda2228c3825f076303d2617c985e74fa6
Abudu Samadu <abudu.samadu@amalitech.com>
2024-12-20 14:24:23 +0000

    feat: add project structure with initial packages .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module team1.testprocessing {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens team1.testprocessing to javafx.fxml;
+    exports team1.testprocessing;
+}
